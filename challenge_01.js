82657af08cf71dba73207c73355c5cf7a464fe1a
Daniel <67779003+DanielH85@users.noreply.github.com>
2020-07-07 15:02:07 +0200

    Challenge01

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,21 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+
+
+var x = prompt("Erste Zahl eingeben:");
+var y = prompt("Zweite Zahl eingeben:");
+
+// Variablentypen ändern
+// Umwandlungsfunktion = keine automatische Umwandlung!
+// Bsp.: parseFloat () = Wandelt Text in einen Fließkommawert um.
+// Achtung: Bei Javascript ist das Fließkomma IMMER ein Punkt!
+
+var summe = parseFloat(x) + parseFloat(y);
+
+console.log(summe);
+
+alert (summe);
+
+output = document.getElementById ("output");
+output.innerHTML = summe;
